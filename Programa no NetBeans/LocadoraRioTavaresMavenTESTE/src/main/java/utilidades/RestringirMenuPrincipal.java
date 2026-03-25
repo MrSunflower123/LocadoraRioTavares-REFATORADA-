3a190443670c21986e440ccf88cb8d6923f36cfc
MrSunflower123 <guiseixas9988@gmail.com>
2026-03-25 17:04:27 -0300

    Commit #5 Adicionado o método desabilitarOpções
    O método desabilitarOpções(), que estava na JFrame MenuPrincipal(), foi movido para a classe restringirMenuPrincipal().

--- Programa no NetBeans/LocadoraRioTavaresMavenTESTE/src/main/java/utilidades/RestringirMenuPrincipal.java
+++ Programa no NetBeans/LocadoraRioTavaresMavenTESTE/src/main/java/utilidades/RestringirMenuPrincipal.java
@@ -1,6 +1,14 @@
 
 package utilidades;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.util.Set;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
 
 /**
  * Classe que vai restringir o acesso dos usuários na tela MenuPrincipal
@@ -9,5 +17,36 @@ package utilidades;
 public class RestringirMenuPrincipal {
     
   
+    /**
+     * Desabilita botões do sistema, de acordo com o tipo de usuário que está conectado
+     * @param comboBox
+     * @param indicesDesabilitados
+     */ 
+    protected static void desabilitarOpcoes(JComboBox<String> comboBox, Set<Integer> indicesDesabilitados) {
     
+    // Customiza o renderizador para mostrar itens desabilitados, mas apenas visualmente
+    comboBox.setRenderer(new DefaultListCellRenderer() {
+        @Override
+        public Component getListCellRendererComponent(
+                JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            if (indicesDesabilitados.contains(index)) {
+                component.setEnabled(false); 
+                component.setForeground(Color.GRAY); 
+            } else {
+                component.setEnabled(true);
+                component.setForeground(Color.BLACK);
+            }
+            return component;
+        }
+    });
+
+    // Impede a seleção de opções desabilitadas
+    comboBox.addActionListener(e -> {
+        if (indicesDesabilitados.contains(comboBox.getSelectedIndex())) {
+            JOptionPane.showMessageDialog(comboBox, "Acesso Negado a esta opção.");
+            comboBox.setSelectedIndex(0); // volta para "Selecione"
+        }
+    });
+}
 }
