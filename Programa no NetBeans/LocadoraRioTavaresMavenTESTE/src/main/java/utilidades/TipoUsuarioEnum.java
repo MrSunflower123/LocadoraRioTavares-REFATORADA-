f44420c49c926795638f0e5e6438d5deb9bddb7f
MrSunflower123 <guiseixas9988@gmail.com>
2026-03-26 15:19:55 -0300

    Commit #12 criado ENUM TipoUsuarioEnum
    Foi criado um arquivo ENUM que define os tipos de usuários que podem acessar o sistema.

---
+++ Programa no NetBeans/LocadoraRioTavaresMavenTESTE/src/main/java/utilidades/TipoUsuarioEnum.java
@@ -0,0 +1,12 @@
+
+package utilidades;
+
+/**
+ * Define os tipos de usuários que podem acessar o sistema
+ * Esses valores são utilizados para verificar as permissões dos usuários
+ * e restringir o acesso as funções do sistema
+ * @author guise
+ */
+public enum TipoUsuarioEnum {
+    
+}
