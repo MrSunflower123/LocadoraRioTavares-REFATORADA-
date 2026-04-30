70d82f6eaefadb4d6316b5a475f57106d349679e
MrSunflower123 <guiseixas9988@gmail.com>
2026-04-30 17:08:56 -0300

    Commit 23# Teste unitários adicionados
    Foi criado um JUnit nomeado TesteCadastroJogos,  no pacote de Test Packages.
    
    Os teste servem para testar o método validarCampos(). Os resultados dos teste foram bem sucedidos.

---
+++ Programa no NetBeans/LocadoraRioTavaresMavenREFATORADA/src/test/java/cadastroJogo/TesteCadastroJogoJUnit.java
@@ -0,0 +1,90 @@
+
+package cadastroJogo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import view.CadastroJogo;
+
+/**
+ *
+ * @author guise
+ */
+public class TesteCadastroJogoJUnit {
+    
+    public TesteCadastroJogoJUnit() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    
+    // Verificando se há um campo vazio
+    // Deve lançar uma exceção se um dos campos estiver vazio
+    // Uma exceção é lançada com uma mensagem específica para aquele campo
+    @Test
+    public void campoVazio(){
+        
+       CadastroJogo cadastro = new CadastroJogo();
+       
+       Exception ex = assertThrows(Exception.class, () -> {
+       
+       cadastro.validarCampos(
+                "", // titulo do jogo vazio
+                "RPG",
+                "PS5",
+                "2000",
+                "Valve",
+                "20"     
+            );
+       });
+        
+       // mensagem específica para o campo  de titulo
+       assertEquals("Campo Titulo não pode ser vazio.", ex.getMessage());
+           
+    }// fecha teste
+    
+    // Verificando se há campos vazios
+    // Não deve lançar exceção se não há nenhum campo vazio
+    @Test
+    public void campoPreenchido() throws Exception{
+        
+       CadastroJogo cadastro = new CadastroJogo();
+         
+       cadastro.validarCampos(
+                "Jogo", // titulo do jogo vazio
+                "RPG",
+                "PS5",
+                "2000",
+                "Valve",
+                "20"     
+            );
+        
+       // mensagem específica para o campo  de titulo
+       assertTrue(true);
+           
+    }// fecha teste
+            
+            
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
